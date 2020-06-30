db5798572d7a109771759b3c71cfba2154f76e52
Annisa Puspitasari <nisa.codelifings@gmail.com>
2020-06-29 21:04:12 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+// JSX
+ReactDOM.render(
+    <div>
+    <h1>Hello Nisa!</h1>
+    <p>aku pasti bisa</p>
+    </div>, document.getElementById("root"))
