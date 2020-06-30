a199504345a400d06d21dbbf32314b7dc9bf4ace
Annisa Puspitasari <annisap.siswa@smkn4bdg.sch.id>
2020-06-29 21:14:14 -0700

    jsx
    ; gausa

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom";
 
-// JSX
-ReactDOM.render(
-    <div>
-    <h1>Hello Nisa!</h1>
-    <p>aku pasti bisa</p>
-    </div>, document.getElementById("root"))
+ReactDOM.render(<ul>
+    <li>banana</li>
+    <li>strawberry</li>
+    <li>watermelon</li></ul>, document.getElementById('root')
+)
